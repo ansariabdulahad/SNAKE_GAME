fef24435b27c8145e0d3c3648e2ffc435a92429b
Sagar ghare <sagar.ghare@acc.ltd>
2023-06-07 10:51:07 +0530

    code done till snake touch his body - game will over

--- index.js
+++ index.js
@@ -74,6 +74,11 @@ const snakeGame = () => {
     for(i = 0; i < snakeBody.length; i++) {
 
         html += `<div class="head" style="grid-area : ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`; // CREATE SNAKE
+
+        // GAME OVER CODE WHILE SNAKE THE BODY
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) { 
+            gameOver = true;
+        }
     }
 
     gameBoard.innerHTML = html;
@@ -81,19 +86,19 @@ const snakeGame = () => {
 
 // DIRECTION FUNCTION CODING
 const direction = (e) => {
-    if(e.key == "ArrowUp") {
+    if(e.key == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
     }
-    else if (e.key == "ArrowDown") {
+    else if (e.key == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
     }
-    else if (e.key == "ArrowLeft") {
+    else if (e.key == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
     }
-    else if (e.key == "ArrowRight") {
+    else if (e.key == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
